b6171730d118b6bb4b7a2d80cd5caf9eef28b6af
Thai Phan <tvphan360@gmail.com>
2022-11-04 13:01:04 -0400

    Add initial integration with Contentful

---
+++ src/components/RichText.tsx
@@ -0,0 +1,33 @@
+import {
+  documentToReactComponents,
+  type Options,
+} from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES, type Document } from "@contentful/rich-text-types";
+import Link from "next/link";
+import Image from "next/image";
+
+interface RichTextProps {
+  field: Document;
+}
+
+export const RichText = (props: RichTextProps) => {
+  return <>{documentToReactComponents(props.field, options)}</>;
+};
+
+const options: Options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      return (
+        <Image
+          alt={node.data.target.fields.title}
+          src={`https:${node.data.target.fields.file.url}`}
+          height={node.data.target.fields.file.details.image.height}
+          width={node.data.target.fields.file.details.image.width}
+        />
+      );
+    },
+    [INLINES.ENTRY_HYPERLINK]: (node, children) => {
+      return <Link href={node.data.target.fields.slug}>{children}</Link>;
+    },
+  },
+};
