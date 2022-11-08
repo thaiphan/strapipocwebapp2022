6b6c478b6318905b3989e73459a5e08d14012668
Thai Phan <tvphan360@gmail.com>
2022-11-08 15:26:50 -0500

    Shift header somewhere else

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import styles from "styles/Home.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { asPath, locale } = router;
+
+  const newLocale = locale === "en" ? "fr" : "en";
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <ul className={styles.nav}>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+          <Link href={asPath} locale={newLocale}>
+            {newLocale.toUpperCase()}
+          </Link>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
