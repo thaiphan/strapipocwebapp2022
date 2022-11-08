264589f40c151d372dae3eabe6b3b209b7f8c5d6
Thai Phan <tvphan360@gmail.com>
2022-11-08 12:21:11 -0500

    Add a initial preview handler

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getEntryBySlug } from "lib/contentful";
+
+export default async function preview(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { secret, slug } = req.query;
+
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  // Fetch the headless CMS to check if the provided `slug` exists
+  const post = await getEntryBySlug(
+    Array.isArray(slug) ? slug.join("/") : slug
+  );
+
+  // If the slug doesn't exist prevent preview mode from being enabled
+  if (!post) {
+    return res.status(401).json({ message: "Invalid slug" });
+  }
+
+  // Enable Preview Mode by setting the cookies
+  res.setPreviewData({});
+
+  res.redirect(307, "/");
+}
